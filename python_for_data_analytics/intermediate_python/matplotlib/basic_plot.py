9c75ec47d2cd5b104efe039f6b98717f88a534a9
kimchi <kimsdev12@gmail.com>
2026-09-14 23:30:15 +0800

    now into matplotlib course and used basics of plot and scatter to visualize the data

---
+++ python_for_data_analytics/intermediate_python/matplotlib/basic_plot.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May']
+sales = [12000, 13500, 9800, 15400, 17200]
+
+plt.plot(months,sales)
+plt.show()
---
+++ python_for_data_analytics/intermediate_python/matplotlib/basic_scatter.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+ad_spend = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+sales = [1200, 1500, 1800, 2100, 2400, 2700, 3000, 3300, 3600, 3900]
+
+plt.scatter(ad_spend,sales)
+plt.xscale('log')
+plt.show()
